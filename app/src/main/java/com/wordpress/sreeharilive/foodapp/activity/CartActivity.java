87d9048bd205b0d0f931c23972f01cdf86e430b8
livin <livinmathew99@gmail.com>
2017-03-25 00:15:23 +0530

    Add to cart complete

--- app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/CartActivity.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/CartActivity.java
@@ -2,14 +2,29 @@ package com.wordpress.sreeharilive.foodapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.wordpress.sreeharilive.foodapp.Cart;
 import com.wordpress.sreeharilive.foodapp.R;
+import com.wordpress.sreeharilive.foodapp.adapter.CartItemsListAdapter;
 
 public class CartActivity extends AppCompatActivity {
+
+    RecyclerView cartRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        cartRecyclerView = (RecyclerView) findViewById(R.id.cartRecyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        cartRecyclerView.setLayoutManager(layoutManager);
+
+        CartItemsListAdapter adapter = new CartItemsListAdapter(this, Cart.getInstance().getCartList());
+        cartRecyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/wordpress/sreeharilive/foodapp/adapter/CartItemsListAdapter.java
@@ -0,0 +1,93 @@
+package com.wordpress.sreeharilive.foodapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.wordpress.sreeharilive.foodapp.R;
+import com.wordpress.sreeharilive.foodapp.activity.FoodDescriptionActivity;
+import com.wordpress.sreeharilive.foodapp.model.FoodItem;
+import com.wordpress.sreeharilive.foodapp.util.Constants;
+
+import java.util.ArrayList;
+
+/***
+ * Created by negibabu on 3/18/17.
+ */
+
+public class CartItemsListAdapter extends RecyclerView.Adapter<CartItemsListAdapter.ViewHolder>{
+
+    private Context context;
+
+    private ArrayList<FoodItem> foodItems;
+
+    public CartItemsListAdapter(Context context, ArrayList<FoodItem> foodItems) {
+        this.context = context;
+        this.foodItems = foodItems;
+    }
+
+    @Override
+    public CartItemsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.cart_list_item,parent,false);
+        return new CartItemsListAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final CartItemsListAdapter.ViewHolder holder, int position) {
+
+
+        holder.nameTextView.setText(
+                foodItems.get(position).getName()
+        );
+
+        holder.priceTextView.setText(
+                String.valueOf(foodItems.get(position).getPrice())
+        );
+
+        holder.rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, FoodDescriptionActivity.class);
+                intent.putExtra(Constants.SELECTED_ITEM_KEY,foodItems.get(holder.getAdapterPosition()));
+                context.startActivity(intent);
+            }
+        });
+
+        holder.qtyTextView.setText(String.valueOf(foodItems.get(position).getQuantity()));
+
+        Picasso.with(context).load(foodItems.get(position).getImageUrl()).into(holder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodItems.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView nameTextView, priceTextView, qtyTextView;
+        View rootView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            imageView = (ImageView) itemView.findViewById(R.id.foodImageView);
+            nameTextView = (TextView) itemView.findViewById(R.id.itemNameTextView);
+            priceTextView = (TextView) itemView.findViewById(R.id.priceTextView);
+            qtyTextView = (TextView) itemView.findViewById(R.id.qtyTextView);
+            rootView = itemView;
+
+        }
+    }
+}
