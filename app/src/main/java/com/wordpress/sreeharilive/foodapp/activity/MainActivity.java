41d9a0dd8890a1e8ae4b47beceab652936b4938c
livin <livinmathew99@gmail.com>
2017-03-24 20:20:53 +0530

    Completed MainActivity

--- app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.wordpress.sreeharilive.foodapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wordpress.sreeharilive.foodapp.R;
 
@@ -11,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchMenu(View view) {
+        Intent intent = new Intent(this,ItemsListActivity.class);
+        switch (view.getId()){
+            case R.id.vegImageView:
+                intent.putExtra("CATEGORY","VEG");
+                break;
+            case R.id.nonVegImageView:
+                intent.putExtra("CATEGORY","NON_VEG");
+                break;
+            case R.id.chineseImageView:
+                intent.putExtra("CATEGORY","CHINESE");
+                break;
+            case R.id.dessertsImageView:
+                intent.putExtra("CATEGORY","DESSERT");
+                break;
+        }
+        startActivity(intent);
+
     }
 }
