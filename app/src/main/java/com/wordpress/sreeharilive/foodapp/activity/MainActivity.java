958b2d5b1737978d989b7a1d1c1366b168acfc04
livin <livinmathew99@gmail.com>
2017-04-08 14:20:20 +0530

    Added NavDrawer

--- app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.wordpress.sreeharilive.foodapp.activity;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,7 +23,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_container);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    super.onBackPressed();
+                }
+                return true;
+            default:
+                return false;
+        }
     }
 
     public void launchMenu(View view) {
@@ -62,4 +94,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void launchHistory(View view) {
+    }
 }
