5c4c5a8bd4762fb23b67123202eae3dff8d9ae45
livin <livinmathew99@gmail.com>
2017-04-08 20:55:37 +0530

    Added Avatar in Drawer

--- app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodapp/activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.wordpress.sreeharilive.foodapp.activity;
 
 import android.content.Intent;
-import android.media.AudioManager;
+import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
